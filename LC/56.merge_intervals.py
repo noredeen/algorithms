68b07632db22830f0373986e169e465b506e254c
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2022-02-19 08:53:06 -0500

    Minor: some comments

--- LC/56.merge_intervals.py
+++ LC/56.merge_intervals.py
@@ -1,7 +1,7 @@
 class Solution:
 
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        intervals.sort(key=lambda interv: interv[0])
+        intervals.sort(key=lambda interv: interv[0])  # merge sort -- O(nlogn)
         merged = []
 
         for interval in intervals:
@@ -11,6 +11,12 @@ class Solution:
             merged[-1][1] = max(merged[-1][1], interval[1])
 
         return merged
+
+    # Follow-up: how do we do this with a large stream of intervals?
+    # i.e we don't have all of them at a time so we can't sort
+
+    def merge_stream(self, intervals: list[list[int]]) -> list[list[int]]:
+        return
 
 
 print(Solution().merge([[1, 3], [8, 10], [2, 6], [15, 18]]))
